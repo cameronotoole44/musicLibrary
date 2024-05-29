da1dddf5719d5d9ad4d73cb42977339e703d3752
cameronotoole44 <cameronotoole404@gmail.com>
2024-05-29 11:29:18 -0400

    react router

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,41 +3,38 @@
 // import GalleryItem from './GalleryItem';
 
 // function Gallery() {
-//     const data = useContext(DataContext)
+//     const data = useContext(DataContext);
 
 //     const display = data.map((item, index) => {
 //         return (
 //             <GalleryItem item={item} key={index} />
-//         )
-//     })
+//         );
+//     });
+
 //     return (
-//         <div>
+//         <div className="gallery">
 //             {display}
 //         </div>
-//     )
-// };
+//     );
+// }
 
 // export default Gallery;
 
 
-import { useContext } from 'react';
-import { DataContext } from '../context/DataContext';
 import GalleryItem from './GalleryItem';
 
-function Gallery() {
-    const data = useContext(DataContext);
+function Gallery(props) {
 
-    const display = data.map((item, index) => {
+    const display = props.data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
-        );
-    });
-
+        )
+    })
     return (
-        <div className="gallery">
+        <div>
             {display}
         </div>
-    );
-}
+    )
+};
 
 export default Gallery;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,42 +1,19 @@
-// import { useContext } from 'react';
-// import { SearchContext } from '../context/SearchContext';
+import { useState } from 'react';
 
-// function Searchbar() {
-//     const { term, handleSearch } = useContext(SearchContext)
-
-//     return (
-//         <form >
-//             <input ref={term} type="text" placeholder="Enter a search term here" />
-//             <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
-//         </form>
-//     )
-// };
-
-// export default Searchbar;
-
-import { useContext } from 'react';
-import { SearchContext } from '../context/SearchContext';
-
-function SearchBar() {
-    const { term, handleSearch } = useContext(SearchContext);
+function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form className="search-form">
-            <input
-                ref={term}
-                type="text"
-                placeholder="Enter a search term here"
-                className="search-input"
-            />
-            <button
-                type="submit"
-                className="search-button"
-                onClick={(e) => handleSearch(e, term.current.value)}
-            >
-                Submit
-            </button>
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
+            <input type="submit" />
+
         </form>
-    );
-}
+    )
+};
 
 export default SearchBar;
